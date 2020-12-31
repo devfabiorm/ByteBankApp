2faa7733f9a24ddb3b57f590b927da79e7500486
devfabiorm <fabio.evangelion.guitar@gmail.com>
2020-12-31 16:34:11 -0300

    Usando base para passar argumentos entre o construtor da classe filha para a classe pai

--- ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/Funcionarios/Diretor.cs
@@ -8,6 +8,11 @@ namespace ByteBank.Funcionarios
 {
     public class Diretor : Funcionario
     {
+        public Diretor(string cpf) : base(cpf)
+        {
+
+        }
+
         public override double GetBonificacao()
         {
             return Salario + base.GetBonificacao();
